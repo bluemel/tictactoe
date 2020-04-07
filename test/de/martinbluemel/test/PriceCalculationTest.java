e749dfb9a2ee95a5745cd2da4969ab0763767df4
Renate Kavuza <Renate Kavuza@RenateKavuza-PC.fritz.box>
2020-04-07 23:09:58 +0200

    junit test für pricecalculatieon verbessert, assertion eingefügt

--- test/de/martinbluemel/test/PriceCalculationTest.java
+++ test/de/martinbluemel/test/PriceCalculationTest.java
@@ -1,10 +1,12 @@
 package de.martinbluemel.test;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class PriceCalculationTest {
 	@Test
 	public void test() {
-		PriceCalculation.calculatePrice(8, 0.1, 0.5);
+		double price = PriceCalculation.calculatePrice(8, 0.1, 0.5);
+		Assert.assertEquals(25.5, price, 0.001);
 	}
 }
